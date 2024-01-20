846a0668094814895cc583d2c2010b33bae1a665
ja-errorpro <jacky8980@gmail.com>
2024-01-20 16:38:04 +0800

    Add buttons for additional services

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:ihousekeeper/permission.dart';
 import 'package:permission_handler/permission_handler.dart';
+import 'package:ihousekeeper/buttons.dart';
 
 void main() {
   runApp(MyApp());
@@ -30,38 +31,38 @@ class MainScreen extends StatelessWidget {
     // p.getPermission(context, Permission.photos);
     return Scaffold(
         appBar: AppBar(title: const Text('iHouseKeeper')),
-        body: Center(
-            child: Row(children: [
-          InkWell(
-            child: const Column(
-              children: [
-                Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: Image(
-                    image: AssetImage('assets/images/water_electricity.png'),
-                    width: 100,
-                  ),
-                ),
-                Text('水電維修')
-              ],
-            ), // Column
-            onTap: () {},
-          ),
-          InkWell(
-            child: const Column(
-              children: [
-                Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: Image(
-                    image: AssetImage('assets/images/house_decoration.jpg'),
-                    width: 100,
-                  ),
-                ),
-                Text('房屋裝修')
-              ],
-            ), // Column
-            onTap: () {},
-          ), // InkWell// InkWell
-        ]))); // Scaffold
+        body: Column(
+          children: [
+            Row(mainAxisAlignment: MainAxisAlignment.center, children: [
+              Buttons.getButton(
+                  '水電維修', 'assets/images/water_electricity.png', () => null),
+              Buttons.getButton(
+                  '房屋裝修', 'assets/images/house_decoration.jpg', () => null),
+              Buttons.getButton('搬家清運',
+                  'assets/images/movingNtransportation.jpg', () => null),
+              Buttons.getButton(
+                  '家事清潔', 'assets/images/house_cleaning.jpg', () => null),
+            ]), // Row
+            Row(mainAxisAlignment: MainAxisAlignment.center, children: [
+              Buttons.getButton(
+                  '交通接送', 'assets/images/transportation.jpg', () => null),
+              Buttons.getButton(
+                  '緊急救援', 'assets/images/emergency.jpg', () => null),
+              Buttons.getButton(
+                  '身體照顧', 'assets/images/body_care.jpg', () => null),
+              Buttons.getButton(
+                  '藥局找', 'assets/images/pharmacy.jpg', () => null),
+            ]), // Row
+            Row(mainAxisAlignment: MainAxisAlignment.center, children: [
+              Buttons.getButton(
+                  '團購找', 'assets/images/group_buying.jpg', () => null),
+              Buttons.getButton(
+                  '成長課程', 'assets/images/growth_course.jpg', () => null),
+              Buttons.getButton('結伴郊遊', 'assets/images/outing.jpg', () => null),
+              Buttons.getButton(
+                  '我的最愛', 'assets/images/my_favourite.jpg', () => null),
+            ]) // Row
+          ],
+        )); // Scaffold
   } // build()
 }
